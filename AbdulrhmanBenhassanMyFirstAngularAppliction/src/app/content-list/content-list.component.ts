bd5501cfd70239a307feb7a6d3d868c50b19090f
abdo754 <123410883+abdo754@users.noreply.github.com>
2024-06-07 15:59:09 -0400

    forgot s step

--- AbdulrhmanBenhassanMyFirstAngularAppliction/src/app/content-list/content-list.component.ts
+++ AbdulrhmanBenhassanMyFirstAngularAppliction/src/app/content-list/content-list.component.ts
@@ -7,20 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ContentListComponent {
   // Define the IContent interface
-  export interface IContent {
-    title: string;
-    imgSrc: string;
-    description: string;
-    author: string;
-    type: string;
-    tags: string[];
-  }
 
   // Array of IContent items
   contents: IContent[] = [
     {
       title: 'Inception',
-      imgSrc: 'path_to_image1.jpg',
+      imgSrc: 'https://m.media-amazon.com/images/M/MV5BZGFjOTRiYjgtYjEzMS00ZjQ2LTkzY2YtOGQ0NDI2NTVjOGFmXkEyXkFqcGdeQXVyNDQ5MDYzMTk@._V1_.jpg',
       description: 'A thief who steals corporate secrets through the use of dream-sharing technology...',
       author: 'Christopher Nolan',
       type: 'Movie',
@@ -28,7 +20,7 @@ export class ContentListComponent {
     },
     {
       title: 'The Matrix',
-      imgSrc: 'path_to_image2.jpg',
+      imgSrc: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
       description: 'A computer hacker learns from mysterious rebels about the true nature of his reality...',
       author: 'Lana Wachowski, Lilly Wachowski',
       type: 'Movie',
@@ -44,7 +36,7 @@ export class ContentListComponent {
     },
     {
       title: 'The Dark Knight',
-      imgSrc: 'path_to_image4.jpg',
+      imgSrc: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
       description: 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc...',
       author: 'Christopher Nolan',
       type: 'Movie',
